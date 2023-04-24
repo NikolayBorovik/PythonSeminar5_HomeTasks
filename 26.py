a430de763dc7ab8085250798f3b3e1611cad76fd
Nikolay Borovik <nik.Borovik@bk.ru>
2023-04-24 12:20:57 +1000

    26 done

--- 26.py
+++ 26.py
@@ -1,3 +1,19 @@
 # Задача 26:  Посчитать факториал (произведение 1 до N) и треугольное число (сумма чисел от 1 до N) 
 # для числа N ЧЕРЕЗ РЕКУРСИЮ и без циклов
 
+def Factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n*Factorial(n-1)
+ 
+def Triangle(n):
+    if n == 1:
+        return n
+    else:
+        return n+Factorial(n-1)
+
+n = int(input('Enter N: '))
+
+print(Factorial(n))
+print(Triangle(n))
